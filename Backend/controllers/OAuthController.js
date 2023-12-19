6a8224f9795b7d0588166d3e4d03df73efbcf4ef
jirayu <jirayu004za@gmail.com>
2023-12-19 14:25:39 +0700

    fix some mistake

--- Backend/controllers/OAuthController.js
+++ Backend/controllers/OAuthController.js
@@ -61,67 +61,76 @@ const OAuthCallback = asyncHandler(async (req, res, next) => {
     res.status(400);
     throw new Error(`Invalid authorization code ${code}`);
   }
-  
-  res.redirect('/signIn/?valid=' + code);
+
+  res.redirect("/signIn/?valid=" + code);
 });
 
 const userInfo = asyncHandler(async (req, res, next) => {
   const code = req.query.valid;
   const access_token = await getOAuthAccessToken(code);
   const user = await getCMUBasicInfo(access_token);
-  //res.json(user);
+  // log cmu basic info from access token
+  console.log("getCMUBasicInfo: ", (user));
 
   //! Maybe can set it to jwt session
   const information = {
     accountType: user.itaccounttype_EN,
-    name: user.cmuitaccount_name,
+    name: user.firstname_EN + " " + user.lastname_EN,
     email: user.cmuitaccount,
     organization: user.organization_name_EN,
     organizationCode: user.organization_code,
-  }
+  };
 
   const { accountType, name, email, organization, organizationCode } = information;
-  /*
-  try {  
-    const newUser = await User.create({
-      accountType,
-      name,
-      email,
-      organization,
-      organizationCode,
-    })
-    res.send(newUser);
-  }catch(error){
-    throw new Error(`Save info user failed: ${error}`);
-  }
-  */
+  // log information about the account
+  console.log("information: ", information);
 
   //! Check if the user already exists
-  try{
-    const existingUser = await User.findOne(
-      {
-        name: user.cmuitaccount_name,
-        email: user.cmuitaccount,
-      });
+  try {
+    const existingUser = await User.findOne({
+      name: user.cmuitaccount_name,
+      email: user.cmuitaccount,
+    });
 
-      if(existingUser){
-        res.json(existingUser);
-        console.log("res.json(existingUser)");
-      }else{
-        //! save to the database
-        const newUser = await User.create({
-          accountType,
-          name,
-          email,
-          organization,
-          organizationCode,
-        })
-        res.json(newUser);
-        console.log("res.json(newUser)");
-      }
-    }catch(error){
-      throw new Error(`Cannot find user || Cannot save user`);
+    if (existingUser) {
+      res.send(existingUser);
+      console.log("existingUser: " + (existingUser));
+    } else {
+      //! save to the database
+      const newUser = await User.create({
+        accountType,
+        name,
+        email,
+        organization,
+        organizationCode,
+      });
+      res.json(newUser);
+      console.log("newUser: " + (newUser));
+    }
+  } catch (error) {
+    console.log(error);
+    //throw new Error(`Cannot find user || Cannot save user`);
   }
+
+  /*
+  try{
+    const user = await User.findOne({
+      name: "jirayu jitprem",
+      email: "jirayu_jitprem@cmu.ac.th",
+    });
+
+    if(user.role === "USER"){
+      await User.updateOne({ role: "ADMIN" });
+      res.json({ role: "ADMIN" });
+      console.log("user role updated to ADMIN");
+    }else{
+      res.json(user);
+      console.log("user role is already ADMIN");
+    }
+  }catch(error){
+    throw new Error(`Cannot update user role`);
+  }
+  */
 });
 
 export { OAuthCallback, userInfo };
